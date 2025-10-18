2777516e904af1f6f4f8cd0acfe8ddb6a408bf9b
Alex Duan <417921451@qq.com>
2025-10-18 14:33:59 +0800

    feat: taosBackup init.

---
+++ tools/taosBackup/src/taosBackup.c
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2025 TAOS Data, Inc. <jhtao@taosdata.com>
+ *
+ * This program is free software: you can use, redistribute, and/or modify
+ * it under the terms of the MIT license as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.
+ */
+    
+#include "taosBackup.h"
+
+int main(int argc, char *argv[]) {
+    printf("taosBackup tool v1.0\n");
+    return 0;
+}
