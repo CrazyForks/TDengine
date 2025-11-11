6dec8a57bf4cd0327b69d3bd9aafe304c1b40b2e
dapan1121 <wpan@taosdata.com>
2025-11-11 14:24:29 +0800

    fix: add debug log

--- source/dnode/vnode/src/meta/metaQuery.c
+++ source/dnode/vnode/src/meta/metaQuery.c
@@ -1613,7 +1613,7 @@ int32_t metaGetTableTags(void *pVnode, uint64_t suid, SArray *pUidTagInfo) {
       metaCloseCtbCursor(pCur);
       return terrno;
     }
-    metaInfo("uid %" PRIu64 " added to uidTagList", uid);
+    metaInfo("uid %" PRIu64 " added to uidTagList, idx:%d", uid, (int32_t)taosArrayGetSize(pUidTagInfo) - 1);
   }
   metaCloseCtbCursor(pCur);
   return TSDB_CODE_SUCCESS;
